b6c2964a0e82e4aa2b2765d7f13146b36c713d92
Dmitry <ImRedBelly@gmail.com>
2021-02-01 21:48:45 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+
+    [SerializeField]
+    private float rotationSpeed = 1f;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+
+    void FixedUpdate()
+    {
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(transform.forward * inputVertical * speed);
+
+        transform.RotateAround(transform.position, Vector3.up, inputHorizontal * rotationSpeed);
+    }
+}
