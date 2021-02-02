033345a84ace6258005f97b61e4e1a8a22463dad
Screwdriver1982 <60478923+Screwdriver1982@users.noreply.github.com>
2021-02-02 20:45:17 +0300

    ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    private void ResetPosition()
+    {
+        //TODO random
+        transform.position = new Vector3(0, 5, 0);
+        rb.velocity = Vector3.zero;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            ResetPosition();
+        }
+        if (collision.gameObject.CompareTag("Hell"))
+        {
+            ResetPosition();
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,58 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 10f;
 
-    [SerializeField]
-    private float rotationSpeed = 1f;
+    [Header("Playre Movement")]
 
-    [SerializeField]
-    private Rigidbody rb;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float rotationSpeed = 1f;
+    [SerializeField] private float jumpForce = 300f;
+
+    [SerializeField] private Rigidbody rb;
 
 
-    void FixedUpdate()
+    [Header("Setting pushing")]
+
+    [SerializeField] private GameObject searchCenter;
+    [SerializeField] private float searchRadius;
+    [SerializeField] private LayerMask searchMask;
+    [SerializeField] private float maxPushForce = 5;
+    [SerializeField] private float pushHeight = 5f;
+    float pushForce;
+    float time;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            time += Time.deltaTime;
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Collider[] balls = Physics.OverlapSphere(searchCenter.transform.position, searchRadius, searchMask);
+            foreach (Collider coll in balls)
+            {
+                Rigidbody ballRb = coll.GetComponent<Rigidbody>();
+
+                Vector3 forceDirection = transform.forward;
+                forceDirection.y = pushHeight;
+                pushForce = ((1 + Mathf.Sin(time)) / 2) * maxPushForce;
+                ballRb.AddForce(forceDirection.normalized * pushForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void FixedUpdate()
     {
         float inputHorizontal = Input.GetAxis("Horizontal");
         float inputVertical = Input.GetAxis("Vertical");
@@ -23,4 +61,12 @@ public class PlayerMovement : MonoBehaviour
 
         transform.RotateAround(transform.position, Vector3.up, inputHorizontal * rotationSpeed);
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(searchCenter.transform.position, searchRadius);
+    }
+
 }
+
