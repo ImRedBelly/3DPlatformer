4143df1e3b2238b585059eedbca0c7ca226909be
Screwdriver1982 <60478923+Screwdriver1982@users.noreply.github.com>
2021-02-25 20:52:44 +0300

    double

--- Assets/Scripts/Player/PlayerAnimationHelper.cs
+++ Assets/Scripts/Player/PlayerAnimationHelper.cs
@@ -5,19 +5,51 @@ using UnityEngine;
 public class PlayerAnimationHelper : MonoBehaviour
 {
     [SerializeField] private MeleeWeapon weapon;
-
+    bool checkCombo;
     Animator animator;
     private void Start()
     {
         animator = GetComponent<Animator>();
     }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (checkCombo)
+            {
+                animator.SetTrigger("Attack1");
+
+            }
+            else
+            {
+                animator.SetTrigger("Attack");
+
+            }
+        }
+
+
+    }
+
+
+
     public void MeleeAttackStart()
     {
+        
         weapon.AttackStart();
     }
     public void MeleeAttackEnd()
     {
         weapon.AttackEnd();
         animator.ResetTrigger("Attack");
+
+    }
+    void CoboStart()
+    {
+        checkCombo = true;
+    }
+    public void ComboEnd()
+    {
+        checkCombo = false;
     }
 }
